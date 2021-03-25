abb0138a6712c5c57d164ef41392d5c2584d466a
na <na@DESKTOP-KV39CP0>
2021-03-25 10:31:18 +0900

    나민엽 컨트롤러 수정함

---
+++ josso-final/src/main/java/com/josso/electronicApproval/controller/ElectronicController.java
@@ -0,0 +1,18 @@
+package com.josso.electronicApproval.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ElectronicController {
+
+	@RequestMapping(value="elecApprovalMain", method=RequestMethod.GET)
+	public ModelAndView approvalMain() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("electronicApproval/electronicApprovalMain");
+		return mv;
+	}
+}
