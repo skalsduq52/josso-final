86eca2ab2749d8e1e0d6a39ba78e3aa6fbe12eae
sangr <sangr@DESKTOP-AJDUEMH>
2021-03-26 14:48:52 +0900

    홍수명 - 마이바티스, 매퍼(staff.vo삭제) / staffController수정

--- josso-final/src/main/java/com/josso/staff/controller/StaffController.java
+++ josso-final/src/main/java/com/josso/staff/controller/StaffController.java
@@ -1,16 +1,18 @@
 package com.josso.staff.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class StaffController {  
 	
 	// 조직도
 	@RequestMapping(value="staff/organization", method=RequestMethod.GET)
 	public ModelAndView organization() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/staff/staffOrganization");
+		mv.setViewName("staff/staffOrganization");
 		return mv;
 	}
 	
@@ -20,7 +22,7 @@ public class StaffController {
 	@RequestMapping(value="staff/address/list", method=RequestMethod.GET)
 	public ModelAndView StaffAddressList() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("/staff/staffAddress");
+		mv.setViewName("staff/staffAddress");
 		return mv;
 	}
 	
