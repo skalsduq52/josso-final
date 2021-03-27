2f75394f0b68a04cf63f9c926a338ddb662d0b49
na <na@DESKTOP-KV39CP0>
2021-03-27 15:15:11 +0900

    나민엽 커밋함

--- josso-final/src/main/java/com/josso/electronicApproval/controller/ElectronicController.java
+++ josso-final/src/main/java/com/josso/electronicApproval/controller/ElectronicController.java
@@ -38,14 +38,19 @@ public class ElectronicController {
 		return mv;
 	}
 	
-	@RequestMapping(value="elecApproval/signing", method=RequestMethod.GET)
+	@RequestMapping(value="elecApproval/signing", method=RequestMethod.POST)
 	public ModelAndView approvalSigning(@RequestParam(name = "middle") String middle,
-			@RequestParam(name = "last") String last) {
-		System.out.println(last);
+										@RequestParam(name = "middlename") String middleName,
+										@RequestParam(name = "last") String last,
+										@RequestParam(name = "lastname") String lastName) {
+		System.out.println("직급 : "+middle);
+		System.out.println("이름 : "+middleName);
+		System.out.println("직급 : "+last);
+		System.out.println("이름 : "+lastName);
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("middle",middle);
-		mv.addObject("last",last);
-		mv.setViewName("/electronicApproval/signing");
+//		mv.addObject("middle",middle);
+//		mv.addObject("last",last);
+//		mv.setViewName("/electronicApproval/signing");
 		return mv;
 	}
 	
