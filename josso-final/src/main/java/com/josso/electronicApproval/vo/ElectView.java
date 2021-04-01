9afef0b5bb9940a707bb9ef45ed94677d6d22f26
na <na@DESKTOP-KV39CP0>
2021-04-01 16:17:13 +0900

    나민엽 커밋

---
+++ josso-final/src/main/java/com/josso/electronicApproval/vo/ElectView.java
@@ -0,0 +1,206 @@
+package com.josso.electronicApproval.vo;
+
+import java.util.Date;
+
+public class ElectView {
+
+	private int rowNum;
+	private String employeeName;
+	private String departmentCode;
+	private String rankCode;
+	private int documentNo;
+	private String documentForm;
+	private String documentName;
+	private Date registerDate;
+	private String startDate;
+	private String endDate;
+	private String drafter;
+	private String drafterAccept;
+	private String middle;
+	private String middleAccept;
+	private String last;
+	private String lastAccept;
+	private String attachedFile;
+	private String holyType;
+	
+	public ElectView() {}
+
+	public ElectView(int rowNum, String employeeName, String departmentCode, String rankCode, int documentNo,
+			String documentForm, String documentName, Date registerDate, String startDate, String endDate,
+			String drafter, String drafterAccept, String middle, String middleAccept, String last, String lastAccept,
+			String attachedFile, String holyType) {
+		this.rowNum = rowNum;
+		this.employeeName = employeeName;
+		this.departmentCode = departmentCode;
+		this.rankCode = rankCode;
+		this.documentNo = documentNo;
+		this.documentForm = documentForm;
+		this.documentName = documentName;
+		this.registerDate = registerDate;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.drafter = drafter;
+		this.drafterAccept = drafterAccept;
+		this.middle = middle;
+		this.middleAccept = middleAccept;
+		this.last = last;
+		this.lastAccept = lastAccept;
+		this.attachedFile = attachedFile;
+		this.holyType = holyType;
+	}
+
+	public int getRowNum() {
+		return rowNum;
+	}
+
+	public void setRowNum(int rowNum) {
+		this.rowNum = rowNum;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getDepartmentCode() {
+		return departmentCode;
+	}
+
+	public void setDepartmentCode(String departmentCode) {
+		this.departmentCode = departmentCode;
+	}
+
+	public String getRankCode() {
+		return rankCode;
+	}
+
+	public void setRankCode(String rankCode) {
+		this.rankCode = rankCode;
+	}
+
+	public int getDocumentNo() {
+		return documentNo;
+	}
+
+	public void setDocumentNo(int documentNo) {
+		this.documentNo = documentNo;
+	}
+
+	public String getDocumentForm() {
+		return documentForm;
+	}
+
+	public void setDocumentForm(String documentForm) {
+		this.documentForm = documentForm;
+	}
+
+	public String getDocumentName() {
+		return documentName;
+	}
+
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDrafter() {
+		return drafter;
+	}
+
+	public void setDrafter(String drafter) {
+		this.drafter = drafter;
+	}
+
+	public String getDrafterAccept() {
+		return drafterAccept;
+	}
+
+	public void setDrafterAccept(String drafterAccept) {
+		this.drafterAccept = drafterAccept;
+	}
+
+	public String getMiddle() {
+		return middle;
+	}
+
+	public void setMiddle(String middle) {
+		this.middle = middle;
+	}
+
+	public String getMiddleAccept() {
+		return middleAccept;
+	}
+
+	public void setMiddleAccept(String middleAccept) {
+		this.middleAccept = middleAccept;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+
+	public String getLastAccept() {
+		return lastAccept;
+	}
+
+	public void setLastAccept(String lastAccept) {
+		this.lastAccept = lastAccept;
+	}
+
+	public String getAttachedFile() {
+		return attachedFile;
+	}
+
+	public void setAttachedFile(String attachedFile) {
+		this.attachedFile = attachedFile;
+	}
+
+	public String getHolyType() {
+		return holyType;
+	}
+
+	public void setHolyType(String holyType) {
+		this.holyType = holyType;
+	}
+
+	@Override
+	public String toString() {
+		return "ElectView [rowNum=" + rowNum + ", employeeName=" + employeeName + ", departmentCode=" + departmentCode
+				+ ", rankCode=" + rankCode + ", documentNo=" + documentNo + ", documentForm=" + documentForm
+				+ ", documentName=" + documentName + ", registerDate=" + registerDate + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", drafter=" + drafter + ", drafterAccept=" + drafterAccept + ", middle="
+				+ middle + ", middleAccept=" + middleAccept + ", last=" + last + ", lastAccept=" + lastAccept
+				+ ", attachedFile=" + attachedFile + ", holyType=" + holyType + "]";
+	}
+	
+}
